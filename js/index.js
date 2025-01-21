3af6aaa1ed69c956b2cdc762d2ac37c520592e27
Desenv-tec <vinicius.jcarlos84@gmail.com>
2025-01-20 23:55:15 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const helloworld = () => {
+    return "Hello World!";
+};
+
+const obj = {
+    prop: {
+        name: "Vinicius",
+        info: {
+            youtube: "/viniciusdocs",
+        },
+    },
+};
